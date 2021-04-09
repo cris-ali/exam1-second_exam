70e1c878b61747fab5dba8774d0bd9431b595d79
cris-ali <calipar@1028business.ph>
2021-04-09 18:40:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-mfjenq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-mfjenq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, VERSION } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+  myForm = {
+    shape: null,
+    color: null,
+    fill: false
+  };
+
+  shapes = [];
+
+  ngOnInit() {}
+
+  submit(data) {
+    // console.log(data.value);
+    this.shapes.push(data.value);
+  }
+}
